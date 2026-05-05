e031daf375b7b44ba25bc50ab35cf36dbbac058c
Rodrigo91218 <pereira97rodrigo@gmail.com>
2026-05-04 21:44:15 -0300

    Agregar archivos de proyecto.

---
+++ actividadHerencia1/Bebida.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace actividadHerencia1
+{
+    internal class Bebida
+    {
+        private bool esAlcoholicaSiNo;
+        private double graduacionAlcoholica;
+        private TipoBebida tipoBebida;
+        private Marca marca;
+    }
+}
