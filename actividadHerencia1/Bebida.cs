68426e12ba5d2098a320c5b0501321d593a3096d
Rodrigo91218 <pereira97rodrigo@gmail.com>
2026-05-05 21:42:42 -0300

    cambios nuevos

--- actividadHerencia1/Bebida.cs
+++ actividadHerencia1/Bebida.cs
@@ -8,9 +8,35 @@ namespace actividadHerencia1
 {
     internal class Bebida
     {
+        #region Atributos
+        private string nombre;
+        private int cm3;
+        private Marca marca;
+        private int cant;
         private bool esAlcoholicaSiNo;
         private double graduacionAlcoholica;
         private TipoBebida tipoBebida;
-        private Marca marca;
+        #endregion
+
+        #region Propiedades
+        public string Nombre {  get { return nombre; } set { nombre = value; } }
+        #endregion
     }
+
+    public class Vino
+    {
+        private int cantCopas;
+
+    }
+
+    public class Cerveza
+    {
+        private int cantLatas;
+    }
+
+    public class AguaSaborizada
+    {
+        private string sabor;
+    }
+
 }
